be5353c2e810db2b00e676afde5e0285d0e9e0b7
Bill Sacks <sacks@ucar.edu>
2026-05-05 05:03:18 -0600

    action=collect dir=gfortranclang_12.2.0_14.0.0_openmpi_g_develop branch=develop hash=v9.0.0b11-21-g73c0487 phase=test

---
+++ develop/gfortranclang/12.2.0_14.0.0/g/openmpi/4.1.4/out/test.bat
@@ -0,0 +1,34 @@
+#!/bin/bash -l
+export JOBID=NO_BATCH
+module load gfortran-12.2.0_clang-14.0.0 4.1.4
+module load netcdf-4.9.0
+
+set -x
+export ESMF_TESTPERFORMANCE=OFF
+export ESMPY_DATA_DIR="/Users/sacks/projects/scratch/esmf-testing/esmf-test-data/grids"
+export ESMF_TEST_NUOPC_JULIA=ON
+export ESMF_DIR=/Users/sacks/projects/scratch/esmf-testing/gfortranclang_12.2.0_14.0.0_openmpi_g_develop/esmf
+export ESMF_COMPILER=gfortranclang
+export ESMF_COMM=openmpi
+export ESMF_NETCDF=nc-config
+export ESMF_BOPT='g'
+export ESMF_TESTEXHAUSTIVE='ON'
+export ESMF_TESTWITHTHREADS='ON'
+module list >& /Users/sacks/projects/scratch/esmf-testing/gfortranclang_12.2.0_14.0.0_openmpi_g_develop/module-test.log
+export WORK_ROOT=/Users/sacks/projects/scratch/esmf-testing/gfortranclang_12.2.0_14.0.0_openmpi_g_develop
+export TEMP_ROOT=/Users/sacks/projects/scratch/esmf-testing/gfortranclang_12.2.0_14.0.0_openmpi_g_develop
+cd $TEMP_ROOT/esmf
+export ESMF_DIR=`pwd`
+make install 2>&1| tee $WORK_ROOT/install.log
+make all_tests 2>&1| tee $WORK_ROOT/test.log
+export ESMFMKFILE=`find $PWD/DEFAULTINSTALLDIR -iname esmf.mk`
+cd ../nuopc-app-prototypes
+./testProtos.sh 2>&1| tee $WORK_ROOT/nuopc.log
+/Users/sacks/projects/scratch/esmf-testing/gfortranclang_12.2.0_14.0.0_openmpi_g_develop/esmpy_install.bat
+cd /Users/sacks/projects/scratch/esmf-testing/gfortranclang_12.2.0_14.0.0_openmpi_g_develop
+conda activate /Users/sacks/projects/scratch/esmf-testing/conda_environments/esmf-test-scripts-environment-python3.11-numpy1.26
+. esmpy_venv/bin/activate
+cd /Users/sacks/projects/scratch/esmf-testing/gfortranclang_12.2.0_14.0.0_openmpi_g_develop/esmf/src/addon/esmpy
+make test 2>&1| tee /Users/sacks/projects/scratch/esmf-testing/gfortranclang_12.2.0_14.0.0_openmpi_g_develop/esmpy-test.log
+deactivate
+conda deactivate
