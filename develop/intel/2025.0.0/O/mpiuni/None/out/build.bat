14587f4be6547bed48a1daf676c2ceb7c067e172
Gerhard Theurich <theurich@sourcespring.net>
2026-05-08 04:10:00 -0400

    action=collect dir=intel_2025.0.0_mpiuni_O_develop branch=develop hash=v9.0.0b12-2-gede6e98 phase=test

---
+++ develop/intel/2025.0.0/O/mpiuni/None/out/build.bat
@@ -0,0 +1,44 @@
+#!/bin/bash -l
+#SBATCH --account=esrl_bmcs
+#SBATCH -o /gpfs/f5/nggps_emc/world-shared/Gerhard.Theurich/ESMF-Nightly-Testing-C5/intel_2025.0.0_mpiuni_O_develop/build.bat_%j.o
+#SBATCH -e /gpfs/f5/nggps_emc/world-shared/Gerhard.Theurich/ESMF-Nightly-Testing-C5/intel_2025.0.0_mpiuni_O_develop/build.bat_%j.e
+#SBATCH --time=1:00:00
+#SBATCH --cluster=c5
+#SBATCH --qos=normal
+#SBATCH --nodes=1
+#SBATCH --ntasks-per-node=128
+#SBATCH --exclusive
+export JOBID=$SLURM_JOBID
+
+
+module load PrgEnv-intel
+module load git
+module load cmake
+module load cray-libsci
+module load intel/2025.0 
+module load cray-hdf5/1.14.3.5 cray-netcdf/4.9.0.17
+
+set -x
+export ESMF_NETCDF_INCLUDE="$CRAY_NETCDF_PREFIX/include"
+export ESMF_NETCDF_LIBPATH="$CRAY_NETCDF_PREFIX/lib"
+export ESMF_NETCDF_LIBS="-lnetcdff -lnetcdf"
+export ESMF_DIR=/gpfs/f5/nggps_emc/world-shared/Gerhard.Theurich/ESMF-Nightly-Testing-C5/intel_2025.0.0_mpiuni_O_develop/esmf
+export ESMF_COMPILER=intel
+export ESMF_COMM=mpiuni
+export ESMF_NETCDF=nc-config
+export ESMF_BOPT='O'
+export ESMF_TESTEXHAUSTIVE='ON'
+export ESMF_TESTWITHTHREADS='ON'
+export ESMF_MPIRUN=/gpfs/f5/nggps_emc/world-shared/Gerhard.Theurich/ESMF-Nightly-Testing-C5/intel_2025.0.0_mpiuni_O_develop/esmf/src/Infrastructure/stubs/mpiuni/mpirun
+module list >& /gpfs/f5/nggps_emc/world-shared/Gerhard.Theurich/ESMF-Nightly-Testing-C5/intel_2025.0.0_mpiuni_O_develop/module-build.log
+export WORK_ROOT=/gpfs/f5/nggps_emc/world-shared/Gerhard.Theurich/ESMF-Nightly-Testing-C5/intel_2025.0.0_mpiuni_O_develop
+export TEMP_ROOT=/tmp/intel_2025.0.0_mpiuni_O_develop
+rsync -a $WORK_ROOT/esmf $TEMP_ROOT
+cd $TEMP_ROOT/esmf
+export ESMF_DIR=`pwd`
+set -o pipefail
+make info 2>&1| tee $WORK_ROOT/info.log
+make -j 128 2>&1| tee $WORK_ROOT/build.log
+cd
+rsync -a $TEMP_ROOT/esmf $WORK_ROOT
+rm -rf $TEMP_ROOT
