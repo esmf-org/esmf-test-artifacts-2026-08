c5e6beb893b0c6bd94b49e81d5904a784b277164
Gerhard Theurich <theurich@sourcespring.net>
2026-05-09 02:09:22 -0600

    action=collect dir=intel_20.0.1_mpiuni_g_develop branch=develop hash=v9.0.0b13 phase=test

---
+++ develop/intel/20.0.1/g/mpiuni/None/out/test.bat
@@ -0,0 +1,28 @@
+#!/bin/bash -l
+#PBS -N test.bat
+#PBS -l walltime=3:00:00
+#PBS -q medium
+#PBS -A UNUSED
+#PBS -l nodes=1:ppn=48
+JOBID="`echo $PBS_JOBID | cut -d. -f1`"
+
+module load compiler/intel/20.0.1 
+module load tool/netcdf/4.7.4/intel/20.0.1
+
+set -x
+export ESMF_DIR=/project/esmf/ESMF-Nightly-Testing-Izumi/intel_20.0.1_mpiuni_g_develop/esmf
+export ESMF_COMPILER=intel
+export ESMF_COMM=mpiuni
+export ESMF_NETCDF=nc-config
+export ESMF_BOPT='g'
+export ESMF_TESTEXHAUSTIVE='ON'
+export ESMF_TESTWITHTHREADS='ON'
+export ESMF_MPIRUN=/project/esmf/ESMF-Nightly-Testing-Izumi/intel_20.0.1_mpiuni_g_develop/esmf/src/Infrastructure/stubs/mpiuni/mpirun
+module list >& /project/esmf/ESMF-Nightly-Testing-Izumi/intel_20.0.1_mpiuni_g_develop/module-test.log
+export WORK_ROOT=/project/esmf/ESMF-Nightly-Testing-Izumi/intel_20.0.1_mpiuni_g_develop
+export TEMP_ROOT=/project/esmf/ESMF-Nightly-Testing-Izumi/intel_20.0.1_mpiuni_g_develop
+cd $TEMP_ROOT/esmf
+export ESMF_DIR=`pwd`
+make install 2>&1| tee $WORK_ROOT/install.log
+make all_tests 2>&1| tee $WORK_ROOT/test.log
+export ESMFMKFILE=`find $PWD/DEFAULTINSTALLDIR -iname esmf.mk`
