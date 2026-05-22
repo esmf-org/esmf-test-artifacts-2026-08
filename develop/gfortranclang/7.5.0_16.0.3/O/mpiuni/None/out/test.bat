0e40df205273f23df8a99a67befb192c3a02dc80
Gerhard Theurich <theurich@sourcespring.net>
2026-05-21 22:27:13 -0700

    action=collect dir=gfortranclang_7.5.0_16.0.3_mpiuni_O_develop branch=develop hash=v9.0.0b13-2-g3a22a06 phase=test

---
+++ develop/gfortranclang/7.5.0_16.0.3/O/mpiuni/None/out/test.bat
@@ -0,0 +1,21 @@
+#!/bin/bash -l
+export JOBID=NO_BATCH
+module load CMake/3.24.3
+module load AOCC/4.1.0 
+
+set -x
+export ESMF_DIR=/home/gerhard/ESMF-Nightly-Testing/gfortranclang_7.5.0_16.0.3_mpiuni_O_develop/esmf
+export ESMF_COMPILER=gfortranclang
+export ESMF_COMM=mpiuni
+export ESMF_BOPT='O'
+export ESMF_TESTEXHAUSTIVE='ON'
+export ESMF_TESTWITHTHREADS='ON'
+export ESMF_MPIRUN=/home/gerhard/ESMF-Nightly-Testing/gfortranclang_7.5.0_16.0.3_mpiuni_O_develop/esmf/src/Infrastructure/stubs/mpiuni/mpirun
+module list >& /home/gerhard/ESMF-Nightly-Testing/gfortranclang_7.5.0_16.0.3_mpiuni_O_develop/module-test.log
+export WORK_ROOT=/home/gerhard/ESMF-Nightly-Testing/gfortranclang_7.5.0_16.0.3_mpiuni_O_develop
+export TEMP_ROOT=/home/gerhard/ESMF-Nightly-Testing/gfortranclang_7.5.0_16.0.3_mpiuni_O_develop
+cd $TEMP_ROOT/esmf
+export ESMF_DIR=`pwd`
+make install 2>&1| tee $WORK_ROOT/install.log
+make all_tests 2>&1| tee $WORK_ROOT/test.log
+export ESMFMKFILE=`find $PWD/DEFAULTINSTALLDIR -iname esmf.mk`
