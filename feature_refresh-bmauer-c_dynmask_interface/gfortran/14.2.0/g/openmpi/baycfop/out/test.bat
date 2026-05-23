e4ce984bdd25c658e4b8082421a5c00dac0a66f5
Bill Sacks <sacks@ucar.edu>
2026-05-23 01:45:09 -0600

    action=collect dir=gfortran_14.2.0_openmpi_g_feature_refresh-bmauer-c_dynmask_interface branch=feature_refresh-bmauer-c_dynmask_interface hash=v9.0.0b13-139-g326dc38 phase=test

---
+++ feature_refresh-bmauer-c_dynmask_interface/gfortran/14.2.0/g/openmpi/baycfop/out/test.bat
@@ -0,0 +1,33 @@
+#!/bin/bash -l
+export JOBID=NO_BATCH
+module load cmake/tw2cvya
+module load gcc/kykcamd openmpi/baycfop
+module load netcdf-c/fwi4w4n netcdf-fortran/etm2w4u
+
+set -x
+export ESMPY_DATA_DIR="/usr/local/esmf/esmf-testing/esmf-test-data/grids"
+export ESMF_DIR=/usr/local/esmf/esmf-testing/gfortran_14.2.0_openmpi_g_feature_refresh-bmauer-c_dynmask_interface/esmf
+export ESMF_COMPILER=gfortran
+export ESMF_COMM=openmpi
+export ESMF_NETCDF=nc-config
+export ESMF_BOPT='g'
+export ESMF_TESTEXHAUSTIVE='ON'
+export ESMF_TESTWITHTHREADS='ON'
+module list >& /usr/local/esmf/esmf-testing/gfortran_14.2.0_openmpi_g_feature_refresh-bmauer-c_dynmask_interface/module-test.log
+export WORK_ROOT=/usr/local/esmf/esmf-testing/gfortran_14.2.0_openmpi_g_feature_refresh-bmauer-c_dynmask_interface
+export TEMP_ROOT=/usr/local/esmf/esmf-testing/gfortran_14.2.0_openmpi_g_feature_refresh-bmauer-c_dynmask_interface
+cd $TEMP_ROOT/esmf
+export ESMF_DIR=`pwd`
+make install 2>&1| tee $WORK_ROOT/install.log
+make all_tests 2>&1| tee $WORK_ROOT/test.log
+export ESMFMKFILE=`find $PWD/DEFAULTINSTALLDIR -iname esmf.mk`
+cd ../nuopc-app-prototypes
+./testProtos.sh 2>&1| tee $WORK_ROOT/nuopc.log
+/usr/local/esmf/esmf-testing/gfortran_14.2.0_openmpi_g_feature_refresh-bmauer-c_dynmask_interface/esmpy_install.bat
+cd /usr/local/esmf/esmf-testing/gfortran_14.2.0_openmpi_g_feature_refresh-bmauer-c_dynmask_interface
+conda activate /usr/local/esmf/esmf-testing/conda_environments/esmf-test-scripts-environment-python3.11-numpy1.26
+. esmpy_venv/bin/activate
+cd /usr/local/esmf/esmf-testing/gfortran_14.2.0_openmpi_g_feature_refresh-bmauer-c_dynmask_interface/esmf/src/addon/esmpy
+make test 2>&1| tee /usr/local/esmf/esmf-testing/gfortran_14.2.0_openmpi_g_feature_refresh-bmauer-c_dynmask_interface/esmpy-test.log
+deactivate
+conda deactivate
