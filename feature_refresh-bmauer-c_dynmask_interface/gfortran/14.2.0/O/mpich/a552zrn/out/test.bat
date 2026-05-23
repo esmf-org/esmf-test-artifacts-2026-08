48e902e5e928d27a007fce0e4cae7df286ff19d9
Bill Sacks <sacks@ucar.edu>
2026-05-23 02:26:30 -0600

    action=collect dir=gfortran_14.2.0_mpich_O_feature_refresh-bmauer-c_dynmask_interface branch=feature_refresh-bmauer-c_dynmask_interface hash=v9.0.0b13-139-g326dc38 phase=test

---
+++ feature_refresh-bmauer-c_dynmask_interface/gfortran/14.2.0/O/mpich/a552zrn/out/test.bat
@@ -0,0 +1,33 @@
+#!/bin/bash -l
+export JOBID=NO_BATCH
+module load cmake/tw2cvya
+module load gcc/kykcamd mpich/a552zrn
+module load netcdf-c/kjkr6dd netcdf-fortran/kcbfb7v
+
+set -x
+export ESMPY_DATA_DIR="/usr/local/esmf/esmf-testing/esmf-test-data/grids"
+export ESMF_DIR=/usr/local/esmf/esmf-testing/gfortran_14.2.0_mpich_O_feature_refresh-bmauer-c_dynmask_interface/esmf
+export ESMF_COMPILER=gfortran
+export ESMF_COMM=mpich
+export ESMF_NETCDF=nc-config
+export ESMF_BOPT='O'
+export ESMF_TESTEXHAUSTIVE='ON'
+export ESMF_TESTWITHTHREADS='ON'
+module list >& /usr/local/esmf/esmf-testing/gfortran_14.2.0_mpich_O_feature_refresh-bmauer-c_dynmask_interface/module-test.log
+export WORK_ROOT=/usr/local/esmf/esmf-testing/gfortran_14.2.0_mpich_O_feature_refresh-bmauer-c_dynmask_interface
+export TEMP_ROOT=/usr/local/esmf/esmf-testing/gfortran_14.2.0_mpich_O_feature_refresh-bmauer-c_dynmask_interface
+cd $TEMP_ROOT/esmf
+export ESMF_DIR=`pwd`
+make install 2>&1| tee $WORK_ROOT/install.log
+make all_tests 2>&1| tee $WORK_ROOT/test.log
+export ESMFMKFILE=`find $PWD/DEFAULTINSTALLDIR -iname esmf.mk`
+cd ../nuopc-app-prototypes
+./testProtos.sh 2>&1| tee $WORK_ROOT/nuopc.log
+/usr/local/esmf/esmf-testing/gfortran_14.2.0_mpich_O_feature_refresh-bmauer-c_dynmask_interface/esmpy_install.bat
+cd /usr/local/esmf/esmf-testing/gfortran_14.2.0_mpich_O_feature_refresh-bmauer-c_dynmask_interface
+conda activate /usr/local/esmf/esmf-testing/conda_environments/esmf-test-scripts-environment-python3.11-numpy1.26
+. esmpy_venv/bin/activate
+cd /usr/local/esmf/esmf-testing/gfortran_14.2.0_mpich_O_feature_refresh-bmauer-c_dynmask_interface/esmf/src/addon/esmpy
+make test 2>&1| tee /usr/local/esmf/esmf-testing/gfortran_14.2.0_mpich_O_feature_refresh-bmauer-c_dynmask_interface/esmpy-test.log
+deactivate
+conda deactivate
