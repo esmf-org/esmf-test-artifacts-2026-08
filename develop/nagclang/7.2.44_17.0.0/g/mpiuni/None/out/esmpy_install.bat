4ee6b74628963131b29f4cc2a52cdfe91dc80d26
Bill Sacks <sacks@ucar.edu>
2026-05-26 19:49:56 -0600

    action=collect dir=nagclang_7.2.44_17.0.0_mpiuni_g_develop branch=develop hash=v9.0.0b13-3-g681a42b phase=test

---
+++ develop/nagclang/7.2.44_17.0.0/g/mpiuni/None/out/esmpy_install.bat
@@ -0,0 +1,28 @@
+#!/bin/bash -l
+module load cmake
+module load None 
+module load netcdf-c/hth7a6z netcdf-fortran/oboy4y3
+
+set -x
+export ESMPY_DATA_DIR="/usr/local/esmf/esmf-testing/esmf-test-data/grids"
+export ESMF_DIR=/usr/local/esmf/esmf-testing/nagclang_7.2.44_17.0.0_mpiuni_g_develop/esmf
+export ESMF_COMPILER=nagclang
+export ESMF_COMM=mpiuni
+export ESMF_NETCDF=nc-config
+export ESMF_BOPT='g'
+export ESMF_TESTEXHAUSTIVE='ON'
+export ESMF_TESTWITHTHREADS='ON'
+export ESMF_MPIRUN=/usr/local/esmf/esmf-testing/nagclang_7.2.44_17.0.0_mpiuni_g_develop/esmf/src/Infrastructure/stubs/mpiuni/mpirun
+cd /usr/local/esmf/esmf-testing/nagclang_7.2.44_17.0.0_mpiuni_g_develop/esmf
+export ESMFMKFILE=`find $PWD/DEFAULTINSTALLDIR -iname esmf.mk`
+/usr/local/esmf/esmf-testing/esmf-test-scripts/py_env_creation/py_env_create --path /usr/local/esmf/esmf-testing/conda_environments --file environment-python3.11-numpy1.26.yml
+cd /usr/local/esmf/esmf-testing/nagclang_7.2.44_17.0.0_mpiuni_g_develop
+conda activate /usr/local/esmf/esmf-testing/conda_environments/esmf-test-scripts-environment-python3.11-numpy1.26
+rm -rf esmpy_venv
+python3 -m venv --system-site-packages esmpy_venv
+. esmpy_venv/bin/activate
+cd /usr/local/esmf/esmf-testing/nagclang_7.2.44_17.0.0_mpiuni_g_develop/esmf/src/addon/esmpy
+python3 -m pip install . 2>&1| tee /usr/local/esmf/esmf-testing/nagclang_7.2.44_17.0.0_mpiuni_g_develop/esmpy-install.log
+make download 2>&1| tee /usr/local/esmf/esmf-testing/nagclang_7.2.44_17.0.0_mpiuni_g_develop/esmpy-download.log
+deactivate
+conda deactivate
