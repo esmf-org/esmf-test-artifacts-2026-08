d9eb3080ea33ee94278695e8ba9c8bbaa49c0a82
Bill Sacks <sacks@ucar.edu>
2026-05-27 02:29:36 -0600

    action=collect dir=intel_2024.2.1_mpi_g_develop branch=develop hash=v9.0.0b13-3-g681a42b phase=test

---
+++ develop/intel/2024.2.1/g/mpi/8.1.29/out/build.bat
@@ -0,0 +1,36 @@
+#!/bin/bash -l
+#PBS -N build.bat
+#PBS -l walltime=2:00:00
+#PBS -q main
+#PBS -A p93300606
+#PBS -l select=1:ncpus=128:mpiprocs=128
+JOBID="`echo $PBS_JOBID | cut -d. -f1`"
+
+module load ncarenv/24.12
+module load intel/2024.2.1
+module load ncarcompilers
+module load cmake
+module load conda/latest
+module load julia
+module load intel/2024.2.1 cray-mpich/8.1.29
+module load netcdf/4.9.3
+
+set -x
+export ESMPY_DATA_DIR="/glade/derecho/scratch/sacks/esmf-testing/esmf-test-data/grids"
+export ESMF_TEST_NUOPC_JULIA=ON
+export ESMF_MPIRUN=mpiexec
+export ESMF_DIR=/glade/derecho/scratch/sacks/esmf-testing/intel_2024.2.1_mpi_g_develop/esmf
+export ESMF_COMPILER=intel
+export ESMF_COMM=mpi
+export ESMF_NETCDF=nc-config
+export ESMF_BOPT='g'
+export ESMF_TESTEXHAUSTIVE='ON'
+export ESMF_TESTWITHTHREADS='ON'
+module list >& /glade/derecho/scratch/sacks/esmf-testing/intel_2024.2.1_mpi_g_develop/module-build.log
+export WORK_ROOT=/glade/derecho/scratch/sacks/esmf-testing/intel_2024.2.1_mpi_g_develop
+export TEMP_ROOT=/glade/derecho/scratch/sacks/esmf-testing/intel_2024.2.1_mpi_g_develop
+cd $TEMP_ROOT/esmf
+export ESMF_DIR=`pwd`
+set -o pipefail
+make info 2>&1| tee $WORK_ROOT/info.log
+make -j 128 2>&1| tee $WORK_ROOT/build.log
