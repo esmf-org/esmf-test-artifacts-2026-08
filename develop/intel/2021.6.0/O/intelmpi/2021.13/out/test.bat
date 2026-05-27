b6037283110a014b03d956dbdd6d784c29674737
Matthew Thompson <matthew.thompson@nasa.gov>
2026-05-27 07:46:31 -0400

    action=collect dir=intel_2021.6.0_intelmpi_O_develop branch=develop hash=v9.0.0b13-3-g681a42b phase=test

---
+++ develop/intel/2021.6.0/O/intelmpi/2021.13/out/test.bat
@@ -0,0 +1,40 @@
+#!/bin/bash -l
+#SBATCH --account=s1873
+#SBATCH -o /discover/nobackup/projects/gmao/SIteam/ESMF_Testing/intel_2021.6.0_intelmpi_O_develop/test.bat_%j.o
+#SBATCH -e /discover/nobackup/projects/gmao/SIteam/ESMF_Testing/intel_2021.6.0_intelmpi_O_develop/test.bat_%j.e
+#SBATCH --time=2:00:00
+#SBATCH --partition=compute
+#SBATCH -C mil
+#SBATCH --qos=allnccs
+#SBATCH --nodes=1
+#SBATCH --ntasks-per-node=120
+#SBATCH --exclusive
+export JOBID=$SLURM_JOBID
+
+
+module use /discover/swdev/gmao_SIteam/modulefiles-SLES15
+module load cmake/3.30.3
+module load comp/gcc/11.4.0
+module load comp/intel/2021.6.0 mpi/impi/2021.13
+
+set -x
+export I_MPI_OFI_PROVIDER=psm3
+export I_MPI_FABRICS=shm:ofi
+export I_MPI_CXX=icpc
+export I_MPI_CC=icc
+export ESMF_DIR=/discover/nobackup/projects/gmao/SIteam/ESMF_Testing/intel_2021.6.0_intelmpi_O_develop/esmf
+export ESMF_COMPILER=intel
+export ESMF_COMM=intelmpi
+export ESMF_BOPT='O'
+export ESMF_TESTEXHAUSTIVE='ON'
+export ESMF_TESTWITHTHREADS='ON'
+module list >& /discover/nobackup/projects/gmao/SIteam/ESMF_Testing/intel_2021.6.0_intelmpi_O_develop/module-test.log
+export WORK_ROOT=/discover/nobackup/projects/gmao/SIteam/ESMF_Testing/intel_2021.6.0_intelmpi_O_develop
+export TEMP_ROOT=/discover/nobackup/projects/gmao/SIteam/ESMF_Testing/intel_2021.6.0_intelmpi_O_develop
+cd $TEMP_ROOT/esmf
+export ESMF_DIR=`pwd`
+make install 2>&1| tee $WORK_ROOT/install.log
+make all_tests 2>&1| tee $WORK_ROOT/test.log
+export ESMFMKFILE=`find $PWD/DEFAULTINSTALLDIR -iname esmf.mk`
+cd ../nuopc-app-prototypes
+./testProtos.sh 2>&1| tee $WORK_ROOT/nuopc.log
