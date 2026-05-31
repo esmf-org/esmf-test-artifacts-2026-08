9ba848363e9bf23688f254aea4e51aed11ba34a2
Gerhard Theurich <theurich@sourcespring.net>
2026-05-31 01:34:45 -0600

    action=collect dir=intel_20.0.1_mpiuni_O_develop branch=develop hash=v9.0.0b14-1-gf183dd1 phase=build

---
+++ develop/intel/20.0.1/O/mpiuni/None/out/build.bat
@@ -0,0 +1,28 @@
+#!/bin/bash -l
+#PBS -N build.bat
+#PBS -l walltime=1:00:00
+#PBS -q medium
+#PBS -A UNUSED
+#PBS -l nodes=1:ppn=48
+JOBID="`echo $PBS_JOBID | cut -d. -f1`"
+
+module load compiler/intel/20.0.1 
+module load tool/netcdf/4.7.4/intel/20.0.1
+
+set -x
+export ESMF_DIR=/project/esmf/ESMF-Nightly-Testing-Izumi/intel_20.0.1_mpiuni_O_develop/esmf
+export ESMF_COMPILER=intel
+export ESMF_COMM=mpiuni
+export ESMF_NETCDF=nc-config
+export ESMF_BOPT='O'
+export ESMF_TESTEXHAUSTIVE='ON'
+export ESMF_TESTWITHTHREADS='ON'
+export ESMF_MPIRUN=/project/esmf/ESMF-Nightly-Testing-Izumi/intel_20.0.1_mpiuni_O_develop/esmf/src/Infrastructure/stubs/mpiuni/mpirun
+module list >& /project/esmf/ESMF-Nightly-Testing-Izumi/intel_20.0.1_mpiuni_O_develop/module-build.log
+export WORK_ROOT=/project/esmf/ESMF-Nightly-Testing-Izumi/intel_20.0.1_mpiuni_O_develop
+export TEMP_ROOT=/project/esmf/ESMF-Nightly-Testing-Izumi/intel_20.0.1_mpiuni_O_develop
+cd $TEMP_ROOT/esmf
+export ESMF_DIR=`pwd`
+set -o pipefail
+make info 2>&1| tee $WORK_ROOT/info.log
+make -j 48 2>&1| tee $WORK_ROOT/build.log
