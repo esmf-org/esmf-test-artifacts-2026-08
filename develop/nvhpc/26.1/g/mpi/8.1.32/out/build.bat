b8e569b969298b6034cb09eb4ff1ca56444d61fd
Bill Sacks <sacks@ucar.edu>
2026-06-02 04:05:15 -0600

    action=collect dir=nvhpc_26.1_mpi_g_develop branch=develop hash=v9.0.0b14-1-gf183dd1 phase=test

---
+++ develop/nvhpc/26.1/g/mpi/8.1.32/out/build.bat
@@ -0,0 +1,31 @@
+#!/bin/bash -l
+#PBS -N build.bat
+#PBS -l walltime=2:00:00
+#PBS -q main
+#PBS -A p93300606
+#PBS -l select=1:ncpus=128:mpiprocs=128
+JOBID="`echo $PBS_JOBID | cut -d. -f1`"
+
+module load ncarenv/25.10
+module load ncarcompilers
+module load cmake
+module load nvhpc/26.1 cray-mpich/8.1.32
+module load netcdf/4.9.3
+
+set -x
+export ESMF_MPIRUN=mpiexec
+export ESMF_DIR=/glade/derecho/scratch/sacks/esmf-testing/nvhpc_26.1_mpi_g_develop/esmf
+export ESMF_COMPILER=nvhpc
+export ESMF_COMM=mpi
+export ESMF_NETCDF=nc-config
+export ESMF_BOPT='g'
+export ESMF_TESTEXHAUSTIVE='ON'
+export ESMF_TESTWITHTHREADS='ON'
+module list >& /glade/derecho/scratch/sacks/esmf-testing/nvhpc_26.1_mpi_g_develop/module-build.log
+export WORK_ROOT=/glade/derecho/scratch/sacks/esmf-testing/nvhpc_26.1_mpi_g_develop
+export TEMP_ROOT=/glade/derecho/scratch/sacks/esmf-testing/nvhpc_26.1_mpi_g_develop
+cd $TEMP_ROOT/esmf
+export ESMF_DIR=`pwd`
+set -o pipefail
+make info 2>&1| tee $WORK_ROOT/info.log
+make -j 128 2>&1| tee $WORK_ROOT/build.log
