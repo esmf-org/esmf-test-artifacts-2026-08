235a3ea16ba8fe469fd0c8f06e314bf061f933f1
Gerhard Theurich <theurich@sourcespring.net>
2026-06-06 01:58:27 -0600

    action=collect dir=gfortran_9.3.0_mpiuni_g_develop branch=develop hash=v9.0.0b14-1-gf183dd1 phase=test

---
+++ develop/gfortran/9.3.0/g/mpiuni/None/out/build.bat
@@ -0,0 +1,28 @@
+#!/bin/bash -l
+#PBS -N build.bat
+#PBS -l walltime=1:00:00
+#PBS -q medium
+#PBS -A UNUSED
+#PBS -l nodes=1:ppn=48
+JOBID="`echo $PBS_JOBID | cut -d. -f1`"
+
+module load compiler/gnu/9.3.0 
+module load tool/netcdf/4.7.4/gnu/9.3.0
+
+set -x
+export ESMF_DIR=/project/esmf/ESMF-Nightly-Testing-Izumi/gfortran_9.3.0_mpiuni_g_develop/esmf
+export ESMF_COMPILER=gfortran
+export ESMF_COMM=mpiuni
+export ESMF_NETCDF=nc-config
+export ESMF_BOPT='g'
+export ESMF_TESTEXHAUSTIVE='ON'
+export ESMF_TESTWITHTHREADS='ON'
+export ESMF_MPIRUN=/project/esmf/ESMF-Nightly-Testing-Izumi/gfortran_9.3.0_mpiuni_g_develop/esmf/src/Infrastructure/stubs/mpiuni/mpirun
+module list >& /project/esmf/ESMF-Nightly-Testing-Izumi/gfortran_9.3.0_mpiuni_g_develop/module-build.log
+export WORK_ROOT=/project/esmf/ESMF-Nightly-Testing-Izumi/gfortran_9.3.0_mpiuni_g_develop
+export TEMP_ROOT=/project/esmf/ESMF-Nightly-Testing-Izumi/gfortran_9.3.0_mpiuni_g_develop
+cd $TEMP_ROOT/esmf
+export ESMF_DIR=`pwd`
+set -o pipefail
+make info 2>&1| tee $WORK_ROOT/info.log
+make -j 48 2>&1| tee $WORK_ROOT/build.log
