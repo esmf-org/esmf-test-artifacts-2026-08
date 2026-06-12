d55379684a75e5d32ce6cb16edfe0a08e0646745
Gerhard Theurich <theurich@sourcespring.net>
2026-06-12 17:07:32 -0600

    action=collect dir=gfortranclang_14.2.0_17.0.0_openmpi_g_fix_cmake-config branch=fix_cmake-config hash=v9.0.0b14-14-g934ef94 phase=test

---
+++ fix_cmake-config/gfortranclang/14.2.0_17.0.0/g/openmpi/jeyhxfm/out/test.bat
@@ -0,0 +1,26 @@
+#!/bin/bash -l
+export JOBID=NO_BATCH
+module load cmake/tw2cvya
+module load gcc/kykcamd openmpi/jeyhxfm
+module load netcdf-c/qop7dbw netcdf-fortran/yg4rpof
+
+set -x
+export ESMPY_DATA_DIR="/usr/local/esmf/esmf-testing/esmf-test-data/grids"
+export ESMF_DIR=/Users/theurich/ESMF-Nightly-Testing/gfortranclang_14.2.0_17.0.0_openmpi_g_fix_cmake-config/esmf
+export ESMF_COMPILER=gfortranclang
+export ESMF_COMM=openmpi
+export ESMF_NETCDF=nc-config
+export ESMF_BOPT='g'
+export ESMF_TESTEXHAUSTIVE='ON'
+export ESMF_TESTWITHTHREADS='ON'
+module list >& /Users/theurich/ESMF-Nightly-Testing/gfortranclang_14.2.0_17.0.0_openmpi_g_fix_cmake-config/module-test.log
+export WORK_ROOT=/Users/theurich/ESMF-Nightly-Testing/gfortranclang_14.2.0_17.0.0_openmpi_g_fix_cmake-config
+export TEMP_ROOT=/Users/theurich/ESMF-Nightly-Testing/gfortranclang_14.2.0_17.0.0_openmpi_g_fix_cmake-config
+cd $TEMP_ROOT/esmf
+export ESMF_DIR=`pwd`
+make install 2>&1| tee $WORK_ROOT/install.log
+make all_tests 2>&1| tee $WORK_ROOT/test.log
+export ESMFMKFILE=`find $PWD/DEFAULTINSTALLDIR -iname esmf.mk`
+export CMAKE_PREFIX_PATH=$(dirname $ESMFMKFILE)/cmake/ESMF:${CMAKE_PREFIX_PATH}
+cd ../nuopc-app-prototypes
+./testProtos.sh 2>&1| tee $WORK_ROOT/nuopc.log
