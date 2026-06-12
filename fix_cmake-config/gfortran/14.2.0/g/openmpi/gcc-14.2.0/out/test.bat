e03c8e8d1a6ebb2b2ce985226c0c41d7c8c8beb2
Gerhard Theurich <theurich@sourcespring.net>
2026-06-12 16:06:00 -0400

    action=collect dir=gfortran_14.2.0_openmpi_g_fix_cmake-config branch=fix_cmake-config hash=v9.0.0b14-13-g81c96e3 phase=test

---
+++ fix_cmake-config/gfortran/14.2.0/g/openmpi/gcc-14.2.0/out/test.bat
@@ -0,0 +1,38 @@
+#!/bin/bash -l
+#SBATCH --account=s3113
+#SBATCH -o /gpfsm/dnb34/gtheuric/ESMF-Nightly-Testing/gfortran_14.2.0_openmpi_g_fix_cmake-config/test.bat_%j.o
+#SBATCH -e /gpfsm/dnb34/gtheuric/ESMF-Nightly-Testing/gfortran_14.2.0_openmpi_g_fix_cmake-config/test.bat_%j.e
+#SBATCH --time=2:00:00
+#SBATCH --partition=compute
+#SBATCH -C mil
+#SBATCH --qos=allnccs
+#SBATCH --nodes=1
+#SBATCH --ntasks-per-node=120
+#SBATCH --exclusive
+export JOBID=$SLURM_JOBID
+
+
+module use /discover/swdev/gmao_SIteam/modulefiles-SLES15
+module load cmake/3.30.3
+module load comp/gcc/14.2.0 mpi/openmpi/4.1.6/gcc-14.2.0
+module load other/esmf-netcdf/gfortran_14.2.0-openmpi_4.1.6
+
+set -x
+export ESMF_DIR=/gpfsm/dnb34/gtheuric/ESMF-Nightly-Testing/gfortran_14.2.0_openmpi_g_fix_cmake-config/esmf
+export ESMF_COMPILER=gfortran
+export ESMF_COMM=openmpi
+export ESMF_NETCDF=nc-config
+export ESMF_BOPT='g'
+export ESMF_TESTEXHAUSTIVE='ON'
+export ESMF_TESTWITHTHREADS='ON'
+module list >& /gpfsm/dnb34/gtheuric/ESMF-Nightly-Testing/gfortran_14.2.0_openmpi_g_fix_cmake-config/module-test.log
+export WORK_ROOT=/gpfsm/dnb34/gtheuric/ESMF-Nightly-Testing/gfortran_14.2.0_openmpi_g_fix_cmake-config
+export TEMP_ROOT=/gpfsm/dnb34/gtheuric/ESMF-Nightly-Testing/gfortran_14.2.0_openmpi_g_fix_cmake-config
+cd $TEMP_ROOT/esmf
+export ESMF_DIR=`pwd`
+make install 2>&1| tee $WORK_ROOT/install.log
+make all_tests 2>&1| tee $WORK_ROOT/test.log
+export ESMFMKFILE=`find $PWD/DEFAULTINSTALLDIR -iname esmf.mk`
+export CMAKE_PREFIX_PATH=$(dirname $ESMFMKFILE)/cmake/ESMF:${CMAKE_PREFIX_PATH}
+cd ../nuopc-app-prototypes
+./testProtos.sh 2>&1| tee $WORK_ROOT/nuopc.log
