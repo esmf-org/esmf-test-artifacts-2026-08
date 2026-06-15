96c796ed7b2cea3dc73c9cb586d21ca0245ced16
Gerhard Theurich <theurich@sourcespring.net>
2026-06-15 17:34:24 -0600

    action=collect dir=nag_7.2.44_mpiuni_g_develop branch=develop hash=v9.0.0b15-1-g2ad1b7a phase=test

---
+++ develop/nag/7.2.44/g/mpiuni/None/out/esmpy_install.bat
@@ -0,0 +1,29 @@
+#!/bin/bash -l
+module load cmake
+module load gcc/kykcamd 
+module load netcdf-c/igrwluw netcdf-fortran/tkdn4yz
+
+set -x
+export FC=nagfor
+export ESMPY_DATA_DIR="/usr/local/esmf/esmf-testing/esmf-test-data/grids"
+export ESMF_DIR=/Users/theurich/ESMF-Nightly-Testing/nag_7.2.44_mpiuni_g_develop/esmf
+export ESMF_COMPILER=nag
+export ESMF_COMM=mpiuni
+export ESMF_NETCDF=nc-config
+export ESMF_BOPT='g'
+export ESMF_TESTEXHAUSTIVE='ON'
+export ESMF_TESTWITHTHREADS='ON'
+export ESMF_MPIRUN=/Users/theurich/ESMF-Nightly-Testing/nag_7.2.44_mpiuni_g_develop/esmf/src/Infrastructure/stubs/mpiuni/mpirun
+cd /Users/theurich/ESMF-Nightly-Testing/nag_7.2.44_mpiuni_g_develop/esmf
+export ESMFMKFILE=`find $PWD/DEFAULTINSTALLDIR -iname esmf.mk`
+/Users/theurich/ESMF-Nightly-Testing/esmf-test-scripts/py_env_creation/py_env_create --path /Users/theurich/ESMF-Nightly-Testing/conda_environments --file environment-python3.11-numpy1.26.yml
+cd /Users/theurich/ESMF-Nightly-Testing/nag_7.2.44_mpiuni_g_develop
+conda activate /Users/theurich/ESMF-Nightly-Testing/conda_environments/esmf-test-scripts-environment-python3.11-numpy1.26
+rm -rf esmpy_venv
+python3 -m venv --system-site-packages esmpy_venv
+. esmpy_venv/bin/activate
+cd /Users/theurich/ESMF-Nightly-Testing/nag_7.2.44_mpiuni_g_develop/esmf/src/addon/esmpy
+python3 -m pip install . 2>&1| tee /Users/theurich/ESMF-Nightly-Testing/nag_7.2.44_mpiuni_g_develop/esmpy-install.log
+make download 2>&1| tee /Users/theurich/ESMF-Nightly-Testing/nag_7.2.44_mpiuni_g_develop/esmpy-download.log
+deactivate
+conda deactivate
