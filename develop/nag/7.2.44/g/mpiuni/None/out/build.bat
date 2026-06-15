412a62a4ec62cfe0667d5d26aee978aec848c8c2
Gerhard Theurich <theurich@sourcespring.net>
2026-06-15 17:19:17 -0600

    action=collect dir=nag_7.2.44_mpiuni_g_develop branch=develop hash=v9.0.0b15-1-g2ad1b7a phase=build

---
+++ develop/nag/7.2.44/g/mpiuni/None/out/build.bat
@@ -0,0 +1,25 @@
+#!/bin/bash -l
+export JOBID=NO_BATCH
+module load cmake
+module load gcc/kykcamd 
+module load netcdf-c/igrwluw netcdf-fortran/tkdn4yz
+
+set -x
+export FC=nagfor
+export ESMPY_DATA_DIR="/usr/local/esmf/esmf-testing/esmf-test-data/grids"
+export ESMF_DIR=/Users/theurich/ESMF-Nightly-Testing/nag_7.2.44_mpiuni_g_develop/esmf
+export ESMF_COMPILER=nag
+export ESMF_COMM=mpiuni
+export ESMF_NETCDF=nc-config
+export ESMF_BOPT='g'
+export ESMF_TESTEXHAUSTIVE='ON'
+export ESMF_TESTWITHTHREADS='ON'
+export ESMF_MPIRUN=/Users/theurich/ESMF-Nightly-Testing/nag_7.2.44_mpiuni_g_develop/esmf/src/Infrastructure/stubs/mpiuni/mpirun
+module list >& /Users/theurich/ESMF-Nightly-Testing/nag_7.2.44_mpiuni_g_develop/module-build.log
+export WORK_ROOT=/Users/theurich/ESMF-Nightly-Testing/nag_7.2.44_mpiuni_g_develop
+export TEMP_ROOT=/Users/theurich/ESMF-Nightly-Testing/nag_7.2.44_mpiuni_g_develop
+cd $TEMP_ROOT/esmf
+export ESMF_DIR=`pwd`
+set -o pipefail
+make info 2>&1| tee $WORK_ROOT/info.log
+make -j 8 2>&1| tee $WORK_ROOT/build.log
