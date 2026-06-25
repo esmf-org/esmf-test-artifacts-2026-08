9d2a0591d5918b8cb54baf7139abe22835e7e2d5
Gerhard Theurich <theurich@sourcespring.net>
2026-06-25 13:38:55 -0600

    action=collect dir=nagclang_7.2.44_17.0.0_openmpi_g_feature_abstract-interfaces branch=feature_abstract-interfaces hash=v9.0.0b15-14-g9258099 phase=test

---
+++ feature_abstract-interfaces/nagclang/7.2.44_17.0.0/g/openmpi/fjdj7gt/out/test.bat
@@ -0,0 +1,34 @@
+#!/bin/bash -l
+export JOBID=NO_BATCH
+module load cmake
+module load None openmpi/fjdj7gt
+module load netcdf-c/d5o6huw netcdf-fortran/7erikgt
+
+set -x
+export ESMPY_DATA_DIR="/usr/local/esmf/esmf-testing/esmf-test-data/grids"
+export ESMF_DIR=/Users/theurich/ESMF-Nightly-Testing/nagclang_7.2.44_17.0.0_openmpi_g_feature_abstract-interfaces/esmf
+export ESMF_COMPILER=nagclang
+export ESMF_COMM=openmpi
+export ESMF_NETCDF=nc-config
+export ESMF_BOPT='g'
+export ESMF_TESTEXHAUSTIVE='ON'
+export ESMF_TESTWITHTHREADS='ON'
+module list >& /Users/theurich/ESMF-Nightly-Testing/nagclang_7.2.44_17.0.0_openmpi_g_feature_abstract-interfaces/module-test.log
+export WORK_ROOT=/Users/theurich/ESMF-Nightly-Testing/nagclang_7.2.44_17.0.0_openmpi_g_feature_abstract-interfaces
+export TEMP_ROOT=/Users/theurich/ESMF-Nightly-Testing/nagclang_7.2.44_17.0.0_openmpi_g_feature_abstract-interfaces
+cd $TEMP_ROOT/esmf
+export ESMF_DIR=`pwd`
+make install 2>&1| tee $WORK_ROOT/install.log
+make all_tests 2>&1| tee $WORK_ROOT/test.log
+export ESMFMKFILE=`find $PWD/DEFAULTINSTALLDIR -iname esmf.mk`
+export CMAKE_PREFIX_PATH=$(dirname $ESMFMKFILE)/cmake/ESMF:${CMAKE_PREFIX_PATH}
+cd ../nuopc-app-prototypes
+./testProtos.sh 2>&1| tee $WORK_ROOT/nuopc.log
+/Users/theurich/ESMF-Nightly-Testing/nagclang_7.2.44_17.0.0_openmpi_g_feature_abstract-interfaces/esmpy_install.bat
+cd /Users/theurich/ESMF-Nightly-Testing/nagclang_7.2.44_17.0.0_openmpi_g_feature_abstract-interfaces
+conda activate /Users/theurich/ESMF-Nightly-Testing/conda_environments/esmf-test-scripts-environment-python3.11-numpy1.26
+. esmpy_venv/bin/activate
+cd /Users/theurich/ESMF-Nightly-Testing/nagclang_7.2.44_17.0.0_openmpi_g_feature_abstract-interfaces/esmf/src/addon/esmpy
+make test 2>&1| tee /Users/theurich/ESMF-Nightly-Testing/nagclang_7.2.44_17.0.0_openmpi_g_feature_abstract-interfaces/esmpy-test.log
+deactivate
+conda deactivate
