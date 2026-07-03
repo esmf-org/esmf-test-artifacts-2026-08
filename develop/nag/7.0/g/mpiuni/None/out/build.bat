0b59f7a03b9dd7ead0c5f44406eb914f1892970a
Gerhard Theurich <theurich@sourcespring.net>
2026-07-03 01:27:56 -0600

    action=collect dir=nag_7.0_mpiuni_g_develop branch=develop hash=v9.0.0b15-17-g9764fd2 phase=build

---
+++ develop/nag/7.0/g/mpiuni/None/out/build.bat
@@ -0,0 +1,29 @@
+#!/bin/bash -l
+#PBS -N build.bat
+#PBS -l walltime=1:00:00
+#PBS -q medium
+#PBS -A UNUSED
+#PBS -l nodes=1:ppn=48
+JOBID="`echo $PBS_JOBID | cut -d. -f1`"
+
+module load compiler/nag/7.0 
+module load tool/netcdf/c4.7.4-f4.5.2/nag-gnu/7.0-9.3.0
+
+set -x
+export ESMF_NETCDFF_LIBS=/usr/local/netcdf-c-4.7.4-f-4.5.2-gcc-g++-9.3.0-nag-7.0/lib/libnetcdff.a
+export ESMF_DIR=/project/esmf/ESMF-Nightly-Testing-Izumi/nag_7.0_mpiuni_g_develop/esmf
+export ESMF_COMPILER=nag
+export ESMF_COMM=mpiuni
+export ESMF_NETCDF=nc-config
+export ESMF_BOPT='g'
+export ESMF_TESTEXHAUSTIVE='ON'
+export ESMF_TESTWITHTHREADS='ON'
+export ESMF_MPIRUN=/project/esmf/ESMF-Nightly-Testing-Izumi/nag_7.0_mpiuni_g_develop/esmf/src/Infrastructure/stubs/mpiuni/mpirun
+module list >& /project/esmf/ESMF-Nightly-Testing-Izumi/nag_7.0_mpiuni_g_develop/module-build.log
+export WORK_ROOT=/project/esmf/ESMF-Nightly-Testing-Izumi/nag_7.0_mpiuni_g_develop
+export TEMP_ROOT=/project/esmf/ESMF-Nightly-Testing-Izumi/nag_7.0_mpiuni_g_develop
+cd $TEMP_ROOT/esmf
+export ESMF_DIR=`pwd`
+set -o pipefail
+make info 2>&1| tee $WORK_ROOT/info.log
+make -j 48 2>&1| tee $WORK_ROOT/build.log
