d1365f569d53003c55ed99ece7ef28758d23b476
Bill Sacks <sacks@ucar.edu>
2026-07-18 08:01:36 -0600

    action=collect dir=gfortran_14.2.0_mpiuni_g_develop branch=develop hash=v9.0.0b16-10-g5ee87db phase=test

---
+++ develop/gfortran/14.2.0/g/mpiuni/None/out/test.bat
@@ -0,0 +1,33 @@
+#!/bin/bash -l
+export JOBID=NO_BATCH
+module load cmake/tw2cvya
+module load gcc/kykcamd 
+module load netcdf-c/gn7eliu netcdf-fortran/cjo2h4h
+
+set -x
+export ESMPY_DATA_DIR="/usr/local/esmf/esmf-testing/esmf-test-data/grids"
+export ESMF_DIR=/usr/local/esmf/esmf-testing/gfortran_14.2.0_mpiuni_g_develop/esmf
+export ESMF_COMPILER=gfortran
+export ESMF_COMM=mpiuni
+export ESMF_NETCDF=nc-config
+export ESMF_BOPT='g'
+export ESMF_TESTEXHAUSTIVE='ON'
+export ESMF_TESTWITHTHREADS='ON'
+export ESMF_MPIRUN=/usr/local/esmf/esmf-testing/gfortran_14.2.0_mpiuni_g_develop/esmf/src/Infrastructure/stubs/mpiuni/mpirun
+module list >& /usr/local/esmf/esmf-testing/gfortran_14.2.0_mpiuni_g_develop/module-test.log
+export WORK_ROOT=/usr/local/esmf/esmf-testing/gfortran_14.2.0_mpiuni_g_develop
+export TEMP_ROOT=/usr/local/esmf/esmf-testing/gfortran_14.2.0_mpiuni_g_develop
+cd $TEMP_ROOT/esmf
+export ESMF_DIR=`pwd`
+make install 2>&1| tee $WORK_ROOT/install.log
+make all_tests 2>&1| tee $WORK_ROOT/test.log
+export ESMFMKFILE=`find $PWD/DEFAULTINSTALLDIR -iname esmf.mk`
+export CMAKE_PREFIX_PATH=$(dirname $ESMFMKFILE)/cmake/ESMF:${CMAKE_PREFIX_PATH}
+/usr/local/esmf/esmf-testing/gfortran_14.2.0_mpiuni_g_develop/esmpy_install.bat
+cd /usr/local/esmf/esmf-testing/gfortran_14.2.0_mpiuni_g_develop
+conda activate /usr/local/esmf/esmf-testing/conda_environments/esmf-test-scripts-environment-python3.11-numpy1.26
+. esmpy_venv/bin/activate
+cd /usr/local/esmf/esmf-testing/gfortran_14.2.0_mpiuni_g_develop/esmf/src/addon/esmpy
+make test 2>&1| tee /usr/local/esmf/esmf-testing/gfortran_14.2.0_mpiuni_g_develop/esmpy-test.log
+deactivate
+conda deactivate
