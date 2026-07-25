448b10a01c192ca0c7d6dce5d0c88e309206a31f
Bill Sacks <sacks@ucar.edu>
2026-07-24 22:59:29 -0600

    action=collect dir=nag_7.2.44_openmpi_g_develop branch=develop hash=v9.0.0b16-13-g6f2cfc7 phase=build

---
+++ develop/nag/7.2.44/g/openmpi/a7f5nwa/out/build.bat
@@ -0,0 +1,24 @@
+#!/bin/bash -l
+export JOBID=NO_BATCH
+module load cmake
+module load gcc/kykcamd openmpi/a7f5nwa
+module load netcdf-c/pywk6et netcdf-fortran/busq63s
+
+set -x
+export FC=nagfor
+export ESMPY_DATA_DIR="/usr/local/esmf/esmf-testing/esmf-test-data/grids"
+export ESMF_DIR=/usr/local/esmf/esmf-testing/nag_7.2.44_openmpi_g_develop/esmf
+export ESMF_COMPILER=nag
+export ESMF_COMM=openmpi
+export ESMF_NETCDF=nc-config
+export ESMF_BOPT='g'
+export ESMF_TESTEXHAUSTIVE='ON'
+export ESMF_TESTWITHTHREADS='ON'
+module list >& /usr/local/esmf/esmf-testing/nag_7.2.44_openmpi_g_develop/module-build.log
+export WORK_ROOT=/usr/local/esmf/esmf-testing/nag_7.2.44_openmpi_g_develop
+export TEMP_ROOT=/usr/local/esmf/esmf-testing/nag_7.2.44_openmpi_g_develop
+cd $TEMP_ROOT/esmf
+export ESMF_DIR=`pwd`
+set -o pipefail
+make info 2>&1| tee $WORK_ROOT/info.log
+make -j 8 2>&1| tee $WORK_ROOT/build.log
