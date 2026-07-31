18d067e749486290c6cbaf235dcc585ffefcd3a2
Matthew Thompson <matthew.thompson@nasa.gov>
2026-07-31 10:49:53 -0400

    action=collect dir=llvm_22.1_openmpi_g_develop branch=develop hash=v9.0.0b16-14-g02ff1b2 phase=build

---
+++ develop/llvm/22.1/g/openmpi/5.0.10/out/build.bat
@@ -0,0 +1,25 @@
+#!/bin/bash -l
+export JOBID=NO_BATCH
+module use /Users/mathomp4/modulefiles/Core
+module load appleclang-flang/22 openmpi/5.0.10
+
+set -x
+export ESMF_TRACE_LIB_BUILD=OFF
+export ESMPY_DATA_DIR="/Users/mathomp4/ESMF_Testing/esmf-test-data/grids"
+export FC="/Users/mathomp4/.homebrew/brew/bin/flang-22"
+export OpenMP_ROOT="/Users/mathomp4/.homebrew/brew/opt/libomp"
+export PATH=/home/mathomp4/.homebrew/brew/bin:$PATH
+export ESMF_DIR=/Users/mathomp4/ESMF_Testing/llvm_22.1_openmpi_g_develop/esmf
+export ESMF_COMPILER=llvm
+export ESMF_COMM=openmpi
+export ESMF_BOPT='g'
+export ESMF_TESTEXHAUSTIVE='ON'
+export ESMF_TESTWITHTHREADS='ON'
+module list >& /Users/mathomp4/ESMF_Testing/llvm_22.1_openmpi_g_develop/module-build.log
+export WORK_ROOT=/Users/mathomp4/ESMF_Testing/llvm_22.1_openmpi_g_develop
+export TEMP_ROOT=/Users/mathomp4/ESMF_Testing/llvm_22.1_openmpi_g_develop
+cd $TEMP_ROOT/esmf
+export ESMF_DIR=`pwd`
+set -o pipefail
+make info 2>&1| tee $WORK_ROOT/info.log
+make -j 6 2>&1| tee $WORK_ROOT/build.log
