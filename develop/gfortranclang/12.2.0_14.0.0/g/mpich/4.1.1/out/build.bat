22d83a0ab55982594c94645f9c6ab1f54e2bafc9
Robert Oehmke <20713427+oehmke@users.noreply.github.com>
2026-08-10 11:36:37 -0600

    action=collect dir=gfortranclang_12.2.0_14.0.0_mpich_g_develop branch=develop hash=v9.0.0b17-4-g17f2be2 phase=build

---
+++ develop/gfortranclang/12.2.0_14.0.0/g/mpich/4.1.1/out/build.bat
@@ -0,0 +1,21 @@
+#!/bin/bash -l
+export JOBID=NO_BATCH
+module load gfortran-12.2.0_clang-14.0.0 4.1.1
+module load netcdf-4.9.2
+
+set -x
+export ESMF_DIR=/Users/oehmke/ESMF_AutoTest/gfortranclang_12.2.0_14.0.0_mpich_g_develop/esmf
+export ESMF_COMPILER=gfortranclang
+export ESMF_COMM=mpich
+export ESMF_NETCDF=nc-config
+export ESMF_BOPT='g'
+export ESMF_TESTEXHAUSTIVE='ON'
+export ESMF_TESTWITHTHREADS='ON'
+module list >& /Users/oehmke/ESMF_AutoTest/gfortranclang_12.2.0_14.0.0_mpich_g_develop/module-build.log
+export WORK_ROOT=/Users/oehmke/ESMF_AutoTest/gfortranclang_12.2.0_14.0.0_mpich_g_develop
+export TEMP_ROOT=/Users/oehmke/ESMF_AutoTest/gfortranclang_12.2.0_14.0.0_mpich_g_develop
+cd $TEMP_ROOT/esmf
+export ESMF_DIR=`pwd`
+set -o pipefail
+make info 2>&1| tee $WORK_ROOT/info.log
+make -j 8 2>&1| tee $WORK_ROOT/build.log
