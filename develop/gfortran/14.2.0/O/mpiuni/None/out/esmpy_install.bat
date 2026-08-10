fd3890bb10d96320f66443b9ac99e8cf2eb9a602
Bill Sacks <sacks@ucar.edu>
2026-08-10 08:22:27 -0600

    action=collect dir=gfortran_14.2.0_mpiuni_O_develop branch=develop hash=v9.0.0b17-4-g17f2be2 phase=test

---
+++ develop/gfortran/14.2.0/O/mpiuni/None/out/esmpy_install.bat
@@ -0,0 +1,28 @@
+#!/bin/bash -l
+module load cmake/tw2cvya
+module load gcc/kykcamd 
+module load netcdf-c/gn7eliu netcdf-fortran/cjo2h4h
+
+set -x
+export ESMPY_DATA_DIR="/usr/local/esmf/esmf-testing/esmf-test-data/grids"
+export ESMF_DIR=/usr/local/esmf/esmf-testing/gfortran_14.2.0_mpiuni_O_develop/esmf
+export ESMF_COMPILER=gfortran
+export ESMF_COMM=mpiuni
+export ESMF_NETCDF=nc-config
+export ESMF_BOPT='O'
+export ESMF_TESTEXHAUSTIVE='ON'
+export ESMF_TESTWITHTHREADS='ON'
+export ESMF_MPIRUN=/usr/local/esmf/esmf-testing/gfortran_14.2.0_mpiuni_O_develop/esmf/src/Infrastructure/stubs/mpiuni/mpirun
+cd /usr/local/esmf/esmf-testing/gfortran_14.2.0_mpiuni_O_develop/esmf
+export ESMFMKFILE=`find $PWD/DEFAULTINSTALLDIR -iname esmf.mk`
+/usr/local/esmf/esmf-testing/esmf-test-scripts/py_env_creation/py_env_create --path /usr/local/esmf/esmf-testing/conda_environments --file environment-python3.11-numpy1.26.yml
+cd /usr/local/esmf/esmf-testing/gfortran_14.2.0_mpiuni_O_develop
+conda activate /usr/local/esmf/esmf-testing/conda_environments/esmf-test-scripts-environment-python3.11-numpy1.26
+rm -rf esmpy_venv
+python3 -m venv --system-site-packages esmpy_venv
+. esmpy_venv/bin/activate
+cd /usr/local/esmf/esmf-testing/gfortran_14.2.0_mpiuni_O_develop/esmf/src/addon/esmpy
+python3 -m pip install . 2>&1| tee /usr/local/esmf/esmf-testing/gfortran_14.2.0_mpiuni_O_develop/esmpy-install.log
+make download 2>&1| tee /usr/local/esmf/esmf-testing/gfortran_14.2.0_mpiuni_O_develop/esmpy-download.log
+deactivate
+conda deactivate
