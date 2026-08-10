d3610ad6f3fe70b54433bfbb202e511288f2a3b4
Bill Sacks <sacks@ucar.edu>
2026-08-10 05:38:20 -0600

    action=collect dir=gfortran_14.2.0_openmpi_g_develop branch=develop hash=v9.0.0b17-4-g17f2be2 phase=test

---
+++ develop/gfortran/14.2.0/g/openmpi/baycfop/out/test.bat
@@ -0,0 +1,34 @@
+#!/bin/bash -l
+export JOBID=NO_BATCH
+module load cmake/tw2cvya
+module load gcc/kykcamd openmpi/baycfop
+module load netcdf-c/fwi4w4n netcdf-fortran/etm2w4u
+
+set -x
+export ESMPY_DATA_DIR="/usr/local/esmf/esmf-testing/esmf-test-data/grids"
+export ESMF_DIR=/usr/local/esmf/esmf-testing/gfortran_14.2.0_openmpi_g_develop/esmf
+export ESMF_COMPILER=gfortran
+export ESMF_COMM=openmpi
+export ESMF_NETCDF=nc-config
+export ESMF_BOPT='g'
+export ESMF_TESTEXHAUSTIVE='ON'
+export ESMF_TESTWITHTHREADS='ON'
+module list >& /usr/local/esmf/esmf-testing/gfortran_14.2.0_openmpi_g_develop/module-test.log
+export WORK_ROOT=/usr/local/esmf/esmf-testing/gfortran_14.2.0_openmpi_g_develop
+export TEMP_ROOT=/usr/local/esmf/esmf-testing/gfortran_14.2.0_openmpi_g_develop
+cd $TEMP_ROOT/esmf
+export ESMF_DIR=`pwd`
+make install 2>&1| tee $WORK_ROOT/install.log
+make all_tests 2>&1| tee $WORK_ROOT/test.log
+export ESMFMKFILE=`find $PWD/DEFAULTINSTALLDIR -iname esmf.mk`
+export CMAKE_PREFIX_PATH=$(dirname $ESMFMKFILE)/cmake/ESMF:${CMAKE_PREFIX_PATH}
+cd ../nuopc-app-prototypes
+./testProtos.sh 2>&1| tee $WORK_ROOT/nuopc.log
+/usr/local/esmf/esmf-testing/gfortran_14.2.0_openmpi_g_develop/esmpy_install.bat
+cd /usr/local/esmf/esmf-testing/gfortran_14.2.0_openmpi_g_develop
+conda activate /usr/local/esmf/esmf-testing/conda_environments/esmf-test-scripts-environment-python3.11-numpy1.26
+. esmpy_venv/bin/activate
+cd /usr/local/esmf/esmf-testing/gfortran_14.2.0_openmpi_g_develop/esmf/src/addon/esmpy
+make test 2>&1| tee /usr/local/esmf/esmf-testing/gfortran_14.2.0_openmpi_g_develop/esmpy-test.log
+deactivate
+conda deactivate
