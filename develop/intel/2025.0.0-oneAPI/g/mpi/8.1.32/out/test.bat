eff4fe082af8138c56415ed3b931f24a766e61e3
Gerhard Theurich <theurich@sourcespring.net>
2026-08-17 18:09:47 -0400

    action=collect dir=intel_2025.0.0-oneAPI_mpi_g_develop branch=develop hash=v8.9.0b12-28-ge6802af phase=all

---
+++ develop/intel/2025.0.0-oneAPI/g/mpi/8.1.32/out/test.bat
@@ -0,0 +1,45 @@
+#!/bin/bash -l
+#SBATCH --account=esrl_bmcs
+#SBATCH -o /gpfs/f5/nggps_emc/world-shared/Gerhard.Theurich/ESMF-Nightly-Testing-C5/intel_2025.0.0-oneAPI_mpi_g_develop/test.bat_%j.o
+#SBATCH -e /gpfs/f5/nggps_emc/world-shared/Gerhard.Theurich/ESMF-Nightly-Testing-C5/intel_2025.0.0-oneAPI_mpi_g_develop/test.bat_%j.e
+#SBATCH --time=2:00:00
+#SBATCH --cluster=c5
+#SBATCH --qos=normal
+#SBATCH --nodes=1
+#SBATCH --ntasks-per-node=128
+#SBATCH --exclusive
+export JOBID=$SLURM_JOBID
+
+
+module load PrgEnv-intel git cmake cray-libsci
+module load intel-oneapi/2025.0 cray-mpich/8.1.32
+module load cray-hdf5/1.14.3.5 cray-netcdf/4.9.0.17
+
+set -x
+export ESMF_NETCDF_INCLUDE="$CRAY_NETCDF_PREFIX/include"
+export ESMF_NETCDF_LIBPATH="$CRAY_NETCDF_PREFIX/lib"
+export ESMF_NETCDF_LIBS="-lnetcdff -lnetcdf"
+export ESMF_MPILAUNCHOPTIONS=--exact
+export ESMF_DIR=/gpfs/f5/nggps_emc/world-shared/Gerhard.Theurich/ESMF-Nightly-Testing-C5/intel_2025.0.0-oneAPI_mpi_g_develop/esmf
+export ESMF_COMPILER=intel
+export ESMF_COMM=mpi
+export ESMF_NETCDF=nc-config
+export ESMF_BOPT='g'
+export ESMF_TESTEXHAUSTIVE='ON'
+export ESMF_TESTWITHTHREADS='ON'
+module list >& /gpfs/f5/nggps_emc/world-shared/Gerhard.Theurich/ESMF-Nightly-Testing-C5/intel_2025.0.0-oneAPI_mpi_g_develop/module-test.log
+export WORK_ROOT=/gpfs/f5/nggps_emc/world-shared/Gerhard.Theurich/ESMF-Nightly-Testing-C5/intel_2025.0.0-oneAPI_mpi_g_develop
+export TEMP_ROOT=/tmp/intel_2025.0.0-oneAPI_mpi_g_develop
+rsync -a $WORK_ROOT/esmf $TEMP_ROOT
+rsync -a $WORK_ROOT/nuopc-app-prototypes $TEMP_ROOT
+cd $TEMP_ROOT/esmf
+export ESMF_DIR=`pwd`
+make install 2>&1| tee $WORK_ROOT/install.log
+make all_tests 2>&1| tee $WORK_ROOT/test.log
+export ESMFMKFILE=`find $PWD/DEFAULTINSTALLDIR -iname esmf.mk`
+cd ../nuopc-app-prototypes
+./testProtos.sh 2>&1| tee $WORK_ROOT/nuopc.log
+cd
+rsync -a $TEMP_ROOT/esmf $WORK_ROOT
+rsync -a $TEMP_ROOT/nuopc-app-prototypes $WORK_ROOT
+rm -rf $TEMP_ROOT
