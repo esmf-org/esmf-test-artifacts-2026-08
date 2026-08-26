86ec27e48fa1c35433857747bda718abc19996cc
Gerhard Theurich <theurich@sourcespring.net>
2026-08-26 00:26:32 -0700

    action=collect dir=gfortranclang_7.5.0_16.0.3_mpiuni_O_develop branch=develop hash=v9.0.0b17-9-g1082792 phase=test

---
+++ develop/gfortranclang/7.5.0_16.0.3/O/mpiuni/None/lib/esmf.mk
@@ -0,0 +1,153 @@
+# ESMF application makefile fragment
+#
+# Use the following ESMF_ variables to compile and link
+# your ESMF application against this ESMF build.
+#
+# !!! VERY IMPORTANT: If the location of this ESMF build is   !!!
+# !!! changed, e.g. libesmf.a is copied to another directory, !!!
+# !!! this file - esmf.mk - must be edited to adjust to the   !!!
+# !!! correct new path                                        !!!
+#
+# Please see end of file for options used on this ESMF build
+#
+
+#----------------------------------------------
+ESMF_VERSION_STRING=9.0.0 beta snapshot
+ESMF_VERSION_STRING_GIT=develop+g1082792c4b
+#----------------------------------------------
+
+ESMF_VERSION_MAJOR=9
+ESMF_VERSION_MINOR=0
+ESMF_VERSION_REVISION=0
+ESMF_VERSION_PATCHLEVEL=0
+ESMF_VERSION_PUBLIC='F'
+ESMF_VERSION_BETASNAPSHOT='T'
+
+
+ESMF_APPSDIR=/home/gerhard/ESMF-Nightly-Testing/gfortranclang_7.5.0_16.0.3_mpiuni_O_develop/esmf/apps/appsO/Linux.gfortranclang.64.mpiuni.default
+ESMF_LIBSDIR=/home/gerhard/ESMF-Nightly-Testing/gfortranclang_7.5.0_16.0.3_mpiuni_O_develop/esmf/lib/libO/Linux.gfortranclang.64.mpiuni.default
+ESMF_ESMXDIR=/home/gerhard/ESMF-Nightly-Testing/gfortranclang_7.5.0_16.0.3_mpiuni_O_develop/esmf/src/addon/ESMX
+ESMF_CMAKEDIR=/home/gerhard/ESMF-Nightly-Testing/gfortranclang_7.5.0_16.0.3_mpiuni_O_develop/esmf/cmake
+
+
+ESMF_F90COMPILER=gfortran
+ESMF_F90LINKER=gfortran
+
+ESMF_F90COMPILEOPTS=-O  -fPIC  -m64 -mcmodel=small -pthread -ffree-line-length-none  -fopenmp
+ESMF_F90COMPILEPATHS=-I/home/gerhard/ESMF-Nightly-Testing/gfortranclang_7.5.0_16.0.3_mpiuni_O_develop/esmf/mod/modO/Linux.gfortranclang.64.mpiuni.default -I/home/gerhard/ESMF-Nightly-Testing/gfortranclang_7.5.0_16.0.3_mpiuni_O_develop/esmf/src/include 
+ESMF_F90COMPILECPPFLAGS=-DESMF_NO_INTEGER_1_BYTE -DESMF_NO_INTEGER_2_BYTE -DESMF_VERSION_STRING_GIT='develop+g1082792c4b' -DESMF_MPIUNI -I/home/gerhard/ESMF-Nightly-Testing/gfortranclang_7.5.0_16.0.3_mpiuni_O_develop/esmf/src/Infrastructure/stubs/mpiuni -DESMF_LOWERCASE_SINGLEUNDERSCORE -DESMF_MOAB=1 -DESMF_LAPACK=1 -DESMF_LAPACK_INTERNAL=1 -DESMF_NO_ACC_SOFTWARE_STACK=1 -DYAML=ESMF_YAML -DESMF_YAMLCPP=1 -DESMF_NO_OPENACC -DESMF_TESTEXHAUSTIVE -DESMF_BOPT_O -DESMF_TESTPERFORMANCE -DESMF_TESTCOMPTUNNEL -DESMF_TESTWITHTHREADS -DSx86_64_small=1 -DESMF_OS_Linux=1 -DESMF_COMM=mpiuni -DESMF_DIR=/home/gerhard/ESMF-Nightly-Testing/gfortranclang_7.5.0_16.0.3_mpiuni_O_develop/esmf
+ESMF_F90COMPILEFREECPP=
+ESMF_F90COMPILEFREENOCPP=-ffree-form
+ESMF_F90COMPILEFIXCPP=-cpp -ffixed-form
+ESMF_F90COMPILEFIXNOCPP=
+
+ESMF_F90LINKOPTS=   -m64 -mcmodel=small -pthread -Wl,--no-as-needed  -fopenmp
+ESMF_F90LINKPATHS=-L/home/gerhard/ESMF-Nightly-Testing/gfortranclang_7.5.0_16.0.3_mpiuni_O_develop/esmf/lib/libO/Linux.gfortranclang.64.mpiuni.default  -L/usr/lib64/gcc/x86_64-suse-linux/7/
+ESMF_F90ESMFLINKPATHS=-L/home/gerhard/ESMF-Nightly-Testing/gfortranclang_7.5.0_16.0.3_mpiuni_O_develop/esmf/lib/libO/Linux.gfortranclang.64.mpiuni.default
+ESMF_F90LINKRPATHS=-Wl,-rpath,/home/gerhard/ESMF-Nightly-Testing/gfortranclang_7.5.0_16.0.3_mpiuni_O_develop/esmf/lib/libO/Linux.gfortranclang.64.mpiuni.default  -Wl,-rpath,/usr/lib64/gcc/x86_64-suse-linux/7/
+ESMF_F90ESMFLINKRPATHS=-Wl,-rpath,/home/gerhard/ESMF-Nightly-Testing/gfortranclang_7.5.0_16.0.3_mpiuni_O_develop/esmf/lib/libO/Linux.gfortranclang.64.mpiuni.default
+ESMF_F90LINKLIBS= -lrt -lstdc++ -ldl
+ESMF_F90ESMFLINKLIBS=-lesmf  -lrt -lstdc++ -ldl
+
+ESMF_CXXCOMPILER=clang++
+ESMF_CXXLINKER=clang++
+
+ESMF_CXXCOMPILEOPTS=-std=c++11 -O -DNDEBUG  -fPIC -m64 -mcmodel=small -pthread  -fopenmp
+ESMF_CXXCOMPILEPATHS= -I/home/gerhard/ESMF-Nightly-Testing/gfortranclang_7.5.0_16.0.3_mpiuni_O_develop/esmf/src/include   -I/home/gerhard/ESMF-Nightly-Testing/gfortranclang_7.5.0_16.0.3_mpiuni_O_develop/esmf/src/prologue/yaml-cpp/include
+ESMF_CXXCOMPILECPPFLAGS=-DESMF_NO_INTEGER_1_BYTE -DESMF_NO_INTEGER_2_BYTE -DESMF_VERSION_STRING_GIT='develop+g1082792c4b' -DESMF_MPIUNI -I/home/gerhard/ESMF-Nightly-Testing/gfortranclang_7.5.0_16.0.3_mpiuni_O_develop/esmf/src/Infrastructure/stubs/mpiuni -DESMF_LOWERCASE_SINGLEUNDERSCORE -DESMF_MOAB=1 -DESMF_LAPACK=1 -DESMF_LAPACK_INTERNAL=1 -DESMF_NO_ACC_SOFTWARE_STACK=1 -DYAML=ESMF_YAML -DESMF_YAMLCPP=1 -DESMF_NO_OPENACC -DESMF_TESTEXHAUSTIVE -DESMF_BOPT_O -DESMF_TESTPERFORMANCE -DESMF_TESTCOMPTUNNEL -DESMF_TESTWITHTHREADS -DSx86_64_small=1 -DESMF_OS_Linux=1 -DESMF_COMM=mpiuni -DESMF_DIR=/home/gerhard/ESMF-Nightly-Testing/gfortranclang_7.5.0_16.0.3_mpiuni_O_develop/esmf -D__SDIR__='' -DESMF_CXXSTD=11 -x c++
+
+ESMF_CXXLINKOPTS=  -m64 -mcmodel=small -pthread -Wl,--no-as-needed  -fopenmp
+ESMF_CXXLINKPATHS=-L/home/gerhard/ESMF-Nightly-Testing/gfortranclang_7.5.0_16.0.3_mpiuni_O_develop/esmf/lib/libO/Linux.gfortranclang.64.mpiuni.default  -L/usr/lib64/gcc/x86_64-suse-linux/7/
+ESMF_CXXESMFLINKPATHS=-L/home/gerhard/ESMF-Nightly-Testing/gfortranclang_7.5.0_16.0.3_mpiuni_O_develop/esmf/lib/libO/Linux.gfortranclang.64.mpiuni.default
+ESMF_CXXLINKRPATHS=-Wl,-rpath,/home/gerhard/ESMF-Nightly-Testing/gfortranclang_7.5.0_16.0.3_mpiuni_O_develop/esmf/lib/libO/Linux.gfortranclang.64.mpiuni.default  -Wl,-rpath,/usr/lib64/gcc/x86_64-suse-linux/7/
+ESMF_CXXESMFLINKRPATHS=-Wl,-rpath,/home/gerhard/ESMF-Nightly-Testing/gfortranclang_7.5.0_16.0.3_mpiuni_O_develop/esmf/lib/libO/Linux.gfortranclang.64.mpiuni.default
+ESMF_CXXLINKLIBS= -lrt -lgfortran -lstdc++ -lm -ldl
+ESMF_CXXESMFLINKLIBS=-lesmf  -lrt -lgfortran -lstdc++ -lm -ldl
+
+ESMF_CCOMPILER=clang
+ESMF_CLINKER=clang
+
+ESMF_CCOMPILEOPTS=-std=c99 -O -DNDEBUG  -fPIC -m64 -mcmodel=small -pthread  -fopenmp
+ESMF_CCOMPILEPATHS=-I/home/gerhard/ESMF-Nightly-Testing/gfortranclang_7.5.0_16.0.3_mpiuni_O_develop/esmf/src/include   -I/home/gerhard/ESMF-Nightly-Testing/gfortranclang_7.5.0_16.0.3_mpiuni_O_develop/esmf/build_config/Linux.gfortranclang.default
+ESMF_CCOMPILECPPFLAGS=-DESMF_NO_INTEGER_1_BYTE -DESMF_NO_INTEGER_2_BYTE -DESMF_VERSION_STRING_GIT='develop+g1082792c4b' -DESMF_MPIUNI -I/home/gerhard/ESMF-Nightly-Testing/gfortranclang_7.5.0_16.0.3_mpiuni_O_develop/esmf/src/Infrastructure/stubs/mpiuni -DESMF_LOWERCASE_SINGLEUNDERSCORE -DESMF_MOAB=1 -DESMF_LAPACK=1 -DESMF_LAPACK_INTERNAL=1 -DESMF_NO_ACC_SOFTWARE_STACK=1 -DYAML=ESMF_YAML -DESMF_YAMLCPP=1 -DESMF_NO_OPENACC -DESMF_TESTEXHAUSTIVE -DESMF_BOPT_O -DESMF_TESTPERFORMANCE -DESMF_TESTCOMPTUNNEL -DESMF_TESTWITHTHREADS -DSx86_64_small=1 -DESMF_OS_Linux=1 -DESMF_COMM=mpiuni -DESMF_DIR=/home/gerhard/ESMF-Nightly-Testing/gfortranclang_7.5.0_16.0.3_mpiuni_O_develop/esmf -D__SDIR__='' -DESMF_CSTD=99
+
+ESMF_CLINKOPTS=  -m64 -mcmodel=small -pthread  -fopenmp
+ESMF_CLINKPATHS=-L/home/gerhard/ESMF-Nightly-Testing/gfortranclang_7.5.0_16.0.3_mpiuni_O_develop/esmf/lib/libO/Linux.gfortranclang.64.mpiuni.default  -L/usr/lib64/gcc/x86_64-suse-linux/7/ -L/home/gerhard/ESMF-Nightly-Testing/gfortranclang_7.5.0_16.0.3_mpiuni_O_develop/esmf/lib/libO/Linux.gfortranclang.64.mpiuni.default  -L/usr/lib64/gcc/x86_64-suse-linux/7/
+ESMF_CESMFLINKPATHS=-L/home/gerhard/ESMF-Nightly-Testing/gfortranclang_7.5.0_16.0.3_mpiuni_O_develop/esmf/lib/libO/Linux.gfortranclang.64.mpiuni.default
+ESMF_CLINKRPATHS=-Wl,-rpath,/home/gerhard/ESMF-Nightly-Testing/gfortranclang_7.5.0_16.0.3_mpiuni_O_develop/esmf/lib/libO/Linux.gfortranclang.64.mpiuni.default -Wl,-rpath,/usr/lib64/gcc/x86_64-suse-linux/7/ -Wl,-rpath,/home/gerhard/ESMF-Nightly-Testing/gfortranclang_7.5.0_16.0.3_mpiuni_O_develop/esmf/lib/libO/Linux.gfortranclang.64.mpiuni.default -Wl,-rpath,/usr/lib64/gcc/x86_64-suse-linux/7/
+ESMF_CESMFLINKRPATHS=-Wl,-rpath,/home/gerhard/ESMF-Nightly-Testing/gfortranclang_7.5.0_16.0.3_mpiuni_O_develop/esmf/lib/libO/Linux.gfortranclang.64.mpiuni.default
+ESMF_CLINKLIBS= -lrt -lgfortran -lstdc++ -lm -ldl  -lrt -lstdc++ -ldl
+ESMF_CESMFLINKLIBS=-lesmf  -lrt -lgfortran -lstdc++ -lm -ldl  -lrt -lstdc++ -ldl
+
+ESMF_SO_F90COMPILEOPTS=-fPIC
+ESMF_SO_F90LINKOPTS=-shared
+ESMF_SO_F90LINKOPTSEXE=-Wl,-export-dynamic
+ESMF_SO_CXXCOMPILEOPTS=-fPIC
+ESMF_SO_CXXLINKOPTS=-shared
+ESMF_SO_CXXLINKOPTSEXE=-Wl,-export-dynamic
+ESMF_SO_CCOMPILEOPTS=-fPIC
+ESMF_SO_CLINKOPTS=-shared
+ESMF_SO_CLINKOPTSEXE=-Wl,-export-dynamic
+
+ESMF_SL_SUFFIX=so
+
+ESMF_OPENMP_F90COMPILEOPTS= -fopenmp
+ESMF_OPENMP_F90LINKOPTS= -fopenmp
+ESMF_OPENMP_CXXCOMPILEOPTS= -fopenmp
+ESMF_OPENMP_CXXLINKOPTS= -fopenmp
+
+ESMF_OPENACC_F90COMPILEOPTS=
+ESMF_OPENACC_F90LINKOPTS=
+ESMF_OPENACC_CXXCOMPILEOPTS=
+ESMF_OPENACC_CXXLINKOPTS=
+
+# ESMF Tracing compile/link options
+ESMF_TRACE_LDPRELOAD=/home/gerhard/ESMF-Nightly-Testing/gfortranclang_7.5.0_16.0.3_mpiuni_O_develop/esmf/lib/libO/Linux.gfortranclang.64.mpiuni.default/libesmftrace_preload.so
+ESMF_TRACE_STATICLINKOPTS=-static -Wl,--wrap=c_esmftrace_notify_wrappers -Wl,--wrap=c_esmftrace_isinitialized -Wl,--wrap=write -Wl,--wrap=writev -Wl,--wrap=pwrite -Wl,--wrap=read -Wl,--wrap=open -Wl,--wrap=MPI_Allgather -Wl,--wrap=MPI_Allgatherv -Wl,--wrap=MPI_Allreduce -Wl,--wrap=MPI_Alltoall -Wl,--wrap=MPI_Alltoallv -Wl,--wrap=MPI_Alltoallw -Wl,--wrap=MPI_Barrier -Wl,--wrap=MPI_Bcast -Wl,--wrap=MPI_Bsend -Wl,--wrap=MPI_Gather -Wl,--wrap=MPI_Gatherv -Wl,--wrap=MPI_Iprobe -Wl,--wrap=MPI_Irecv -Wl,--wrap=MPI_Irsend -Wl,--wrap=MPI_Isend -Wl,--wrap=MPI_Issend -Wl,--wrap=MPI_Probe -Wl,--wrap=MPI_Recv -Wl,--wrap=MPI_Reduce -Wl,--wrap=MPI_Rsend -Wl,--wrap=MPI_Scan -Wl,--wrap=MPI_Scatter -Wl,--wrap=MPI_Scatterv -Wl,--wrap=MPI_Send -Wl,--wrap=MPI_Sendrecv -Wl,--wrap=MPI_Test -Wl,--wrap=MPI_Testall -Wl,--wrap=MPI_Testany -Wl,--wrap=MPI_Testsome -Wl,--wrap=MPI_Wait -Wl,--wrap=MPI_Waitall -Wl,--wrap=MPI_Waitany -Wl,--wrap=MPI_Waitsome -Wl,--wrap=mpi_allgather_ -Wl,--wrap=mpi_allgather__ -Wl,--wrap=mpi_allgatherv_ -Wl,--wrap=mpi_allgatherv__ -Wl,--wrap=mpi_allreduce_ -Wl,--wrap=mpi_allreduce__ -Wl,--wrap=mpi_alltoall_ -Wl,--wrap=mpi_alltoall__ -Wl,--wrap=mpi_alltoallv_ -Wl,--wrap=mpi_alltoallv__ -Wl,--wrap=mpi_alltoallw_ -Wl,--wrap=mpi_alltoallw__ -Wl,--wrap=mpi_barrier_ -Wl,--wrap=mpi_barrier__ -Wl,--wrap=mpi_bcast_ -Wl,--wrap=mpi_bcast__ -Wl,--wrap=mpi_bsend_ -Wl,--wrap=mpi_bsend__ -Wl,--wrap=mpi_exscan_ -Wl,--wrap=mpi_exscan__ -Wl,--wrap=mpi_gather_ -Wl,--wrap=mpi_gather__ -Wl,--wrap=mpi_gatherv_ -Wl,--wrap=mpi_gatherv__ -Wl,--wrap=mpi_iprobe_ -Wl,--wrap=mpi_iprobe__ -Wl,--wrap=mpi_irecv_ -Wl,--wrap=mpi_irecv__ -Wl,--wrap=mpi_irsend_ -Wl,--wrap=mpi_irsend__ -Wl,--wrap=mpi_isend_ -Wl,--wrap=mpi_isend__ -Wl,--wrap=mpi_issend_ -Wl,--wrap=mpi_issend__ -Wl,--wrap=mpi_probe_ -Wl,--wrap=mpi_probe__ -Wl,--wrap=mpi_recv_ -Wl,--wrap=mpi_recv__ -Wl,--wrap=mpi_reduce_ -Wl,--wrap=mpi_reduce__ -Wl,--wrap=mpi_reduce_scatter_ -Wl,--wrap=mpi_reduce_scatter__ -Wl,--wrap=mpi_rsend_ -Wl,--wrap=mpi_rsend__ -Wl,--wrap=mpi_scatter_ -Wl,--wrap=mpi_scatter__ -Wl,--wrap=mpi_scatterv_ -Wl,--wrap=mpi_scatterv__ -Wl,--wrap=mpi_scan_ -Wl,--wrap=mpi_scan__ -Wl,--wrap=mpi_send_ -Wl,--wrap=mpi_send__ -Wl,--wrap=mpi_sendrecv_ -Wl,--wrap=mpi_sendrecv__ -Wl,--wrap=mpi_test_ -Wl,--wrap=mpi_test__ -Wl,--wrap=mpi_testall_ -Wl,--wrap=mpi_testall__ -Wl,--wrap=mpi_testany_ -Wl,--wrap=mpi_testany__ -Wl,--wrap=mpi_testsome_ -Wl,--wrap=mpi_testsome__ -Wl,--wrap=mpi_wait_ -Wl,--wrap=mpi_wait__ -Wl,--wrap=mpi_waitall_ -Wl,--wrap=mpi_waitall__ -Wl,--wrap=mpi_waitany_ -Wl,--wrap=mpi_waitany__
+ESMF_TRACE_STATICLINKLIBS=-lesmftrace_static
+ESMF_TRACE_DYNAMICLINKLIBS=-lesmftrace_preload
+ESMF_F90ESMFPRELOADLINKLIBS=-lesmf -lesmftrace_preload  -lrt -lstdc++ -ldl
+
+# Internal ESMF variables, do NOT depend on these!
+
+ESMF_INTERNAL_DIR=/home/gerhard/ESMF-Nightly-Testing/gfortranclang_7.5.0_16.0.3_mpiuni_O_develop/esmf
+ESMF_INTERNAL_MPIRUN="/home/gerhard/ESMF-Nightly-Testing/gfortranclang_7.5.0_16.0.3_mpiuni_O_develop/esmf/src/Infrastructure/stubs/mpiuni/mpirun"
+
+#
+# !!! The following options were used on this ESMF build !!!
+#
+# ESMF_DIR: /home/gerhard/ESMF-Nightly-Testing/gfortranclang_7.5.0_16.0.3_mpiuni_O_develop/esmf
+# ESMF_OS: Linux
+# ESMF_MACHINE: x86_64
+# ESMF_ABI: 64
+# ESMF_COMPILER: gfortranclang
+# ESMF_BOPT: O
+# ESMF_COMM: mpiuni
+# ESMF_SITE: default
+# ESMF_PTHREADS: ON
+# ESMF_OPENMP: ON
+# ESMF_OPENACC: OFF
+# ESMF_ARRAY_LITE: FALSE
+# ESMF_NO_INTEGER_1_BYTE: TRUE
+# ESMF_NO_INTEGER_2_BYTE: TRUE
+# ESMF_FORTRANSYMBOLS: default
+# ESMF_MAPPER_BUILD: OFF
+# ESMF_AUTO_LIB_BUILD: ON
+# ESMF_DEFER_LIB_BUILD: ON
+# ESMF_SHARED_LIB_BUILD: ON
+# 
+# ESMF environment variables pointing to 3rd party software:
+# ESMF_MOAB:              internal
+# ESMF_LAPACK:            internal
+# ESMF_ACC_SOFTWARE_STACK:            none
+# ESMF_YAMLCPP:           internal
+#
+# * Compilers, Linkers, Flags, and Libraries *
+# Location of the preprocessor:      /opt/aocc/aocc-compiler-4.1.0/bin/clang
+# Location of the Fortran compiler:  /usr/bin/gfortran
+# Location of the Fortran linker:    /usr/bin/gfortran
+# Location of the C++ compiler:      /opt/aocc/aocc-compiler-4.1.0/bin/clang++
+# Location of the C++ linker:        /opt/aocc/aocc-compiler-4.1.0/bin/clang++
+# Location of the C compiler:        /opt/aocc/aocc-compiler-4.1.0/bin/clang
+# Location of the C linker:          /opt/aocc/aocc-compiler-4.1.0/bin/clang
+#
